586e9ebbe3cfed4a116e541f0b658dbec7b93fd0
Aryan Naraghi <aryan.naraghi@gmail.com>
2026-03-29 11:55:51 -0700

    Makes more updates to the bitboard

--- src/Bitboard.tsx
+++ src/Bitboard.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 type BitboardProps = {
   children: string;
   showBits?: boolean;
 };
 
-export const Bitboard = (props: BitboardProps) => {
+const parse = (input: string) => {
   const board = [];
-  for (const char of props.children) {
+  for (const char of input) {
     if (char == ".") {
       board.push(false);
     } else if (char == "X") {
@@ -19,28 +19,65 @@ export const Bitboard = (props: BitboardProps) => {
     throw Error("Invalid bitboard.");
   }
 
+  return board;
+};
+
+export const Bitboard = (props: BitboardProps) => {
+  const board = parse(props.children);
+
+  const [hovered, setHovered] = useState<number | null>(null);
+  const [selected, setSelected] = useState<number | null>(null);
+
+  const renderCell = (index: number, content: string | number) => {
+    const isActive = index === hovered || index == selected;
+
+    return (
+      <span
+        style={{
+          cursor: "pointer",
+          display: "inline-block",
+          borderRadius: "4px",
+          backgroundColor: isActive ? "yellow" : "transparent",
+          fontWeight: isActive ? "bold" : "normal",
+          color: isActive ? "black" : "inherit", // Bright blue
+          transform: isActive ? "scale(2)" : undefined,
+        }}
+        onMouseOver={() => setHovered(index)}
+        onMouseLeave={() => setHovered(null)}
+        onClick={() => setSelected(index === selected ? null : index)}
+      >
+        {content}
+      </span>
+    );
+  };
+
   return (
     <code>
       <pre>
         {board.map((value, index) => (
-          <React.Fragment key={`cell-${index}`}>
+          <React.Fragment key={`square-${index}`}>
             {index % 8 === 0 && `${8 - index / 8}:`}{" "}
-            <span>{value ? "X" : "."}</span>
+            {renderCell(index, value ? "X" : ".")}
             {index % 8 === 7 && "\n"}
           </React.Fragment>
         ))}
 
         {"   a b c d e f g h"}
 
-        {props.showBits && "\n\n"}
+        {props.showBits && "\n\n "}
 
         {props.showBits &&
-          board.toReversed().map((value, index) => (
-            <React.Fragment key={`cell-${index}`}>
-              <span>{value ? 1 : 0}</span>
-              {index % 8 === 7 && "\n"}
-            </React.Fragment>
-          ))}
+          board.toReversed().map((value, reversedIndex) => {
+            const originalIndex = 64 - reversedIndex - 1;
+
+            return (
+              <React.Fragment key={`bit-${originalIndex}`}>
+                {reversedIndex % 8 === 0 && "  "}
+                {renderCell(originalIndex, value ? 1 : 0)}
+                {originalIndex % 8 === 0 && "\n"}{" "}
+              </React.Fragment>
+            );
+          })}
       </pre>
     </code>
   );
